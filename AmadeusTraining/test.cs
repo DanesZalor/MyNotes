850c1b1b314521c79b9e1c5324923ef9791f1d5c
Daniel James DOLOR <danieljames.dolor@amadeus.com>
2022-10-27 08:01:25 +0800

    a

---
+++ AmadeusTraining/test.cs
@@ -0,0 +1,24 @@
+ var reservations = new Reservation[] {
+new Reservation(
+        "5J", 100,
+        new StationCode("CDO", new StationCode("DVO",
+        DateOnly.MaxValue, new Passenger[] {
+            new Passenger("Morde","cai", DateOnly.Parse("01/20/1999"),
+            new Passenger("Rig","by", DateOnly.Parse("07/15/1999")
+        }
+),
+new Reservation(
+    "5J", 8801,
+    new StationCode("DVO", new StationCode("NGR",
+    DateOnly.MaxValue, new Passenger[] {
+        new Passenger("amongus","balls", DateOnly.Parse("05/12/2000")
+    }
+),
+new Reservation(
+    "5J", 100,
+    new StationCode("CDO", new StationCode("DVO",
+    DateOnly.MaxValue, new Passenger[] {
+    new Passenger("Your","Mom", DateOnly.MinValue),
+    new Passenger("Mao","Zhedong", DateOnly.Parse("12/26/1893")
+    }
+)
